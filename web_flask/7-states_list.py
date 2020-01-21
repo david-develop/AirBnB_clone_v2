ae766dc3d9ba4488a09c297d99c83d7a7a627c44
david-develop <924@holbertonschool.com>
2020-01-21 15:50:13 -0500

    [Fix] render options

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -10,13 +10,13 @@ from models.state import State
 app = Flask(__name__)
 all_states = storage.all(State)
 states_name_id = {value.id: value.name for (key, value) in all_states.items()}
-sort_list = dict(sorted(states_name_id.items(), key = lambda x : x[1]))
+
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """display a HTML page with all states and cities"""
     return render_template('7-states_list.html', class_n="States",
-                           all_states=sort_list)
+                           all_states=states_name_id)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
