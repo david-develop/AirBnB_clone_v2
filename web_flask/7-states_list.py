c5a6b9202c9176a21927b5373dd2ecef64d8a035
david-develop <924@holbertonschool.com>
2020-01-21 16:58:30 -0500

    [Fix] sorting option

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,21 +8,19 @@ from models.state import State
 
 
 app = Flask(__name__)
-all_states = storage.all(State)
-states_name_id = {value.id: value.name for (key, value) in all_states.items()}
-
-
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """display a HTML page with all states and cities"""
-    return render_template('7-states_list.html', class_n="States",
-                           all_states=states_name_id)
 
 
 @app.teardown_appcontext
 def teardown_storage(self):
     """Close storage session"""
     storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """display a HTML page with all states and cities"""
+    all_states = storage.all(State).values()
+    return render_template('7-states_list.html', all_states=all_states)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
