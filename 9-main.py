d30934e7c9baf0edb2bfff16a54f11093ebfc51f
david-develop <924@holbertonschool.com>
2020-01-22 12:02:05 -0500

    [Add] files for HBNB is alive page flask script and template [Fix] 100-dump.sql script wrong words

--- 9-main.py
+++ 9-main.py
@@ -5,12 +5,13 @@
 from models import storage
 from models.state import State
 from models.city import City
-
+from models.place import Place
 
 print("")
-all_states = storage.all(State).values()
-print(all_states)
+all_places = storage.all(Place).values()
 #for state in all_states:
 #    if state.id == '421a55f4-7d82-47d9-b54c-a76916479552':
 #        for city in state.cities:
 #            print("Find the city {} in the state {} with state_id {}".format(city.name, state.name, state.id))
+for place in all_places:
+    print("Name: {} -> Description: {}, id: {}".format(place.name, place.description, place.id))
